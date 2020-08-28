535f0cefca9a0c22c71cd8a82a780637abc4cc59
feserr <feserr3@gmail.com>
2020-08-28 19:59:23 +0100

    Add missing change directory for Unix CI.

--- ci/build_unix.sh
+++ ci/build_unix.sh
@@ -5,4 +5,5 @@ workspace=`pwd`
 mkdir -p build
 cmake -DCMAKE_BUILD_TYPE=Release -H${workspace} -B${workspace}/build -G "Ninja"
 cmake --build ${workspace}/build --config Release --target all -- -j 10
+cd build
 ctest -C Release
