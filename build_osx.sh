daeb60fcd9e8459320b0f32f37393c6738825be5
feserr <feserr3@gmail.com>
2020-08-28 17:41:31 +0100

    Add simple gtest.

--- build_osx.sh
+++ build_osx.sh
@@ -5,4 +5,4 @@ workspace=`pwd`
 mkdir -p build
 cmake -DCMAKE_BUILD_TYPE=Release -H${workspace} -B${workspace}/build -G "Ninja"
 cmake --build ${workspace}/build --config Release --target all -- -j 10
-# ctest -C Release
+ctest -C Release
---
+++ unittests/utils_test.cpp
@@ -0,0 +1,20 @@
+/*
+ * Copyright 2020 Javier Jorge. All rights reserved.
+ * License: BSD 2-Clause License
+ */
+
+#include <Utils.h>
+
+#include <vector>
+
+#include "gtest/gtest.h"
+
+TEST(Utils, RobustAddTest) {
+  ASSERT_EQ(robust_add(std::vector<float>(10, 0), 0.0f, 10),
+            robust_add(std::vector<float>(10, 0), 1.0f, 10));
+}
+
+int main(int argc, char** argv) {
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
