efc32f50e9728889f4eea167121c405da2eb105c
feserr <feserr@users.noreply.github.com>
2020-08-29 00:17:43 +0100

    Add simple Google UnitTests
    Run the test on CI.

--- ci/build_unix.sh
+++ ci/build_unix.sh
@@ -5,4 +5,5 @@ workspace=`pwd`
 mkdir -p build
 cmake -DCMAKE_BUILD_TYPE=Release -H${workspace} -B${workspace}/build -G "Ninja"
 cmake --build ${workspace}/build --config Release --target all -- -j 10
-# ctest -C Release
+cd build
+ctest -C Release
