fd1b307d80861b1ae38904174d4d3f2acfc4e989
feserr <feserr3@gmail.com>
2020-08-28 18:03:30 +0100

    Use CMake to run the tests.

--- ci/build_osx.sh
+++ ci/build_osx.sh
@@ -5,6 +5,4 @@ workspace=`pwd`
 mkdir -p build
 cmake -DCMAKE_BUILD_TYPE=Release -H${workspace} -B${workspace}/build -G "Ninja"
 cmake --build ${workspace}/build --config Release --target all -- -j 10
-cd build
-ctest -C Release
-cd ..
+cmake --build ${workspace}/build --config Release --target ContinuousTest
