92427c152c5c05166121575288d8416793990097
JJorgeDSIC <jjorge@dsic.upv.es>
2020-09-09 10:52:51 +0200

    WIP decoder, trying with unique_ptr

--- cppdecoder/main.cpp
+++ cppdecoder/main.cpp
@@ -1,25 +1,29 @@
 #include <AcousticModel.h>
-#include <DGaussianAcousticModel.h>
 #include <Decoder.h>
 #include <MixtureAcousticModel.h>
 #include <Sample.h>
 #include <SearchGraphLanguageModel.h>
-#include <TiedStatesAcousticModel.h>
 #include <Utils.h>
 
-int main() {
-  SearchGraphLanguageModel sgraph;
-  sgraph.read_model("/home/jjorge/PyDecoder/models/2.gram.graph");
+#include <memory>
 
-  MixtureAcousticModel* mixturemodel = new MixtureAcousticModel(
-      "/home/jjorge/PyDecoder/models/monophone_model_I32");
+int main() {
+  std::unique_ptr<SearchGraphLanguageModel> sgraph(
+      new SearchGraphLanguageModel());
+  sgraph->read_model("/home/jjorge/PyDecoder/models/2.gram.graph");
+
+  std::unique_ptr<AcousticModel> mixturemodel(new MixtureAcousticModel(
+      "/home/jjorge/PyDecoder/models/monophone_model_I32"));
+
+  std::cout << mixturemodel->getDim() << std::endl;
+
+  Decoder decoder(sgraph, mixturemodel);
 
   Sample sample;
 
   sample.read_sample("/home/jjorge/PyDecoder/samples/AAFA0016.features");
 
-  std::cout << sample.getDim() << std::endl;
   std::cout << sample.getNFrames() << std::endl;
 
-  Decoder decoder(sgraph, mixturemodel, sample);
+  std::cout << decoder.decode(sample) << std::endl;
 }
